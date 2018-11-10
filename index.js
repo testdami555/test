d89ad20103751cde2968a073ca1d684e47743564
unknown <testdami555@gmail.com>
2018-11-09 21:11:41 -0300

    first commit otra vez

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("Hola Mundo MUY Feliz ...");
     res.end();
-    console.log("Hola de la consola");
 }).listen(80);
