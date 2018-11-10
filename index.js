9d17d2d921a968c16eea8786cc5cdf530f16c201
unknown <testdami555@gmail.com>
2018-11-09 21:29:41 -0300

    first commit otra vez

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Hola Mundo MUY Feliz ...");
-    res.end();
-}).listen(80);
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
