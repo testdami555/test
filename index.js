6d52ee63626b6b18d87650764c07ea3131bd0e70
unknown <testdami555@gmail.com>
2018-11-13 10:25:21 -0300

    Procfile2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require('http');
-const PORT = process.env.PORT || 5000;
+const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
+server.listen(port, () => {
+  console.log(`Server running on ${port}/`);
 });
