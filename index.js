1b4c0a9afa18dcc6204599770f5c93267d92d314
testdami555 <testdami555@gmail.com>
2020-07-25 15:17:38 -0300

    Cambios en el index.js

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const http = require('http');
 const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<p>Hello World....</p>');
+  res.write('<p>Hello World.</p>');
+  res.write('<script>console.log("Soy la consola de Chrome")</script>');
+  res.end();
 });
 server.listen(port, () => {
-  console.log(`Server running on ${port}/`);
+  console.log(`Server running on ${port}`);
 });
